944996f6618b33acda644277a2649a2822aecbda
indankit124 <indankit124@gmail.com>
2025-07-20 17:12:35 +0530

    Api level Data Sanitization

--- models/user.js
+++ models/user.js
@@ -4,11 +4,15 @@ const userSchema = new mongoose.Schema({
     firstName:{
         type: String,},
         lastName:{type:String},
-        emailId:{type:String , required:true, unique:true},
-        password:{type:String, required:true},
+        emailId:{type:String , required:true, unique:true,trim:true,lowercase:true},
+        password:{type:String, required:true,minLength:6,maxLength:15},
         age:{type:Number, min :18,max:120},
-        gender:{type:String,required:true}
-}
+        gender:{type:String,required:true,validate(value){if(!["male","female","others"]){throw new console.error();
+        }}},
+        photoUrl:{type:String,default:"https://cdn.pixabay.com/photo/2015/03/04/22/35/avatar-659651_1280.png"},
+        about:{type:String,default:"this is a default about String"},
+        skills:{type:String}
+},{timestamps:true}
 );  
 
 const userModule = mongoose.model("User",userSchema);
--- src/app.js
+++ src/app.js
@@ -4,21 +4,77 @@ const DBConnect= require("../config/DataBase.js");
 const userModule = require("../models/user.js")
 const app = express();
 app.use(express.json())
-app.post("/signup" ,async(req,res)=>{
-// const userObj ={
-//     firstName:"Ankit",
-//     lastName:"Mahala",
-//     emailId:"ankitMsddahala@gmail.com",
-//     password:"ankitMahala@1234",
-//     age:24,
-//     gender:"Male"
-// };
-// console.log(req.body)
+
+
+app.post("/user" ,async(req,res)=>{
+console.log(req.body)
 const user = new userModule(req.body);
 await user.save();
 res.send("user added")
+});
+
+//FEED API - get all the users from the api for the feed
+app.get("/feed",async(req, res)=>{
+console.log(req.body)
+const users = await userModule.find(req.body); // Find all users  with a email id 
+        // res.status(200).json(users);
+        if(users.length>0){
+        res.send(users)}
+    else{res.send("User not find")
+
+    }}
+        // catch{res.send("User not find")}
+)
+
+
+
+
+
+
+app.get ("/findingUser",async (req,res)=>{
+   const alluser= await userModule.find() ;
+   res.send(alluser)
 })
 
+
+
+
+
+
+app.get("/allUser",async(req, res)=>{
+console.log(req.body)
+const users = await userModule.findOne(req.body); // Find all users  with a email id 
+        // res.status(200).json(users);
+        if(users==null){
+        res.send("User not find")}
+    else{res.send(users)
+  
+    }}
+        // catch{res.send("User not find")}
+)
+
+
+
+
+
+
+app.patch("/user/:user_id",async(req,res)=>{
+    
+   const data= req.body;
+   const  userId= req.params.user_id;
+    try{
+    const ALLOWED_UPDADTES=["firstName","lastName","password","age","gender","photoUrl","about","skills"];
+    const is_update_allowed = Object.keys(data).every((k)=>ALLOWED_UPDADTES.includes(k))   ;
+     if(!is_update_allowed){
+        throw new Error("update not allowed")
+     }
+    const updatedUser = await userModule.findByIdAndUpdate(userId ,data,{runValidators:true,returnDocument:"after"});
+   
+    res.send("user is updated")}
+    catch(err){res.status(404).send(err.message)}
+   
+    
+})
 DBConnect().then(()=>{console.log("connection to the db");app.listen(3000,()=>{
     console.log("Server is running on port 3000")} )}).catch((err)=>{"Not connected ",err});
 
