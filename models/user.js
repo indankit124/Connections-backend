489f7e33cedc95238ac38e16d2d74e075d74779a
indankit124 <indankit124@gmail.com>
2025-07-23 16:04:16 +0530

    Encrypting Passwords

--- models/user.js
+++ models/user.js
@@ -1,17 +1,19 @@
 const mongoose = require('mongoose');
+const validator = require('validator')
 
 const userSchema = new mongoose.Schema({
     firstName:{
-        type: String,},
-        lastName:{type:String},
-        emailId:{type:String , required:true, unique:true,trim:true,lowercase:true},
-        password:{type:String, required:true,minLength:6,maxLength:15},
-        age:{type:Number, min :18,max:120},
-        gender:{type:String,required:true,validate(value){if(!["male","female","others"]){throw new console.error();
+        type: String,validate(value){if(value.length<4){throw new Error("name is too short not valid");
         }}},
-        photoUrl:{type:String,default:"https://cdn.pixabay.com/photo/2015/03/04/22/35/avatar-659651_1280.png"},
+        lastName:{type:String},
+        emailId:{type:String , required:true, unique:true,trim:true,lowercase:true, validate(value){if(!validator.isEmail(value)){throw new Error("enter valid email id ")}}},
+        password:{type:String, required:true,minLength:6,maxLength:105},
+        age:{type:Number, min :18,max:120},
+        gender:{type:String,required:true,validate(value){if(!["male","female","others"].includes(value.toLowerCase())){throw new Error("invalid error")}
+        }},
+        photoUrl:{type:String,default:"https://cdn.pixabay.com/photo/2015/03/04/22/35/avatar-659651_1280.png",validate(value){if(!validator.isURL(value)){throw new Error("invalid photo url")}}},
         about:{type:String,default:"this is a default about String"},
-        skills:{type:String}
+        skills:{type:[String],default:[]}
 },{timestamps:true}
 );  
 
--- src/app.js
+++ src/app.js
@@ -2,28 +2,87 @@ const express = require("express");
 const {AdminAuth,UserAuth}=require("../middlewares/authentication.js")
 const DBConnect= require("../config/DataBase.js");
 const userModule = require("../models/user.js")
+const validateSignUpData = require("../utils/validation.js")
+const bcrypt = require("bcrypt");
+
 const app = express();
 app.use(express.json())
 
 
-app.post("/user" ,async(req,res)=>{
+app.post("/signup" ,async(req,res)=>{
 console.log(req.body)
-const user = new userModule(req.body);
-await user.save();
-res.send("user added")
-});
+try{
+    validateSignUpData(req);
+    
+    
+    
+  let {firstName ,lastName,emailId,password,gender} = req.body;
+   
+  
+  const saltRounds = 10;
+     const myPlaintextPassword = password;
+     const passwordHash = await bcrypt.hash(myPlaintextPassword, saltRounds);
+        // Store hash in your password DB.    
+        console.log("Hash password:" ,passwordHash)
 
-//FEED API - get all the users from the api for the feed
+       password = passwordHash;
+    
+    // Store hash in your password DB.
+
+
+  //creating a new instance of userModule with the data from the request body
+// and saving it to the database  
+    const user = new userModule({firstName ,lastName,emailId,password,gender});
+await user.save();
+res.send("user added")}
+ catch (err) {
+    console.error("Validation failed:", err.message);
+    res.status(404).send({ error: err.message })
+}});
+
+
+
+
+
+
+
+app.post("/login", async (req, res) => {
+  console.log(req.body);
+  const {emailId,password}= req.body;
+  const checkingUser = await userModule.findOne({emailId:req.body.emailId});
+  console.log(checkingUser)
+  if(!checkingUser){
+    res.send("user not register ")
+  } 
+ 
+  const isPasswordCorrect = await bcrypt.compare(password, checkingUser.password);
+   // res.send("got crerdential- user logged in  ");
+  // console.log(isPasswordCorrect)
+    
+if (!isPasswordCorrect) {
+  return res.status(401).send("Invalid password");
+}
+
+res.send("Login successful");
+})
+
+
+
+
+
+
+
+
 app.get("/feed",async(req, res)=>{
 console.log(req.body)
-const users = await userModule.find(req.body); // Find all users  with a email id 
-        // res.status(200).json(users);
+const users = await userModule.find(req.body); 
+   
         if(users.length>0){
         res.send(users)}
     else{res.send("User not find")
 
     }}
-        // catch{res.send("User not find")}
+    
 )
 
 
@@ -43,14 +102,14 @@ app.get ("/findingUser",async (req,res)=>{
 
 app.get("/allUser",async(req, res)=>{
 console.log(req.body)
-const users = await userModule.findOne(req.body); // Find all users  with a email id 
-        // res.status(200).json(users);
+const users = await userModule.findOne(req.body); 
+        
         if(users==null){
         res.send("User not find")}
     else{res.send(users)
   
     }}
-        // catch{res.send("User not find")}
+       
 )
 
 
